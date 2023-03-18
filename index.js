e474e6fe986c02ae265f36d7addb286fddf630bf
Brian <bfeddes@hotmail.com>
2023-03-18 12:23:02 -0500

    updated

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-// requiring
 const express = require('express');
 const { MongoClient } = require('mongodb');
 
 const app = express();
 
-const uri = 'mongodb+srv://brianfeddes:NetflixPassword@netflixdatabase.m4ijrna.mongodb.net/?retryWrites=true&w=majority'; // connection string
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const uri = 'mongodb+srv://brianfeddes:NetflixPassword@netflixdatabase.m4ijrna.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(express.static(__dirname + '/client'))
-
-// redirecting so the default page will be the home page
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/client/index.html')
-});
-
-// getting all movies and shows from all streaming services
-app.get('/total', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     await client.connect();
     const collection = client.db('NetflixDatabase').collection('NetflixCollection');
     const data = await collection.find({}).toArray();
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(data));
-  } catch (e) {
-    console.error(e);
-    res.status(500).send('There was an error on the server.');
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   } finally {
     await client.close();
   }
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log('Server is running on port 3000');
 });
